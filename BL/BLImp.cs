c164ebe3f1b8742e674afe6880f08dcba57c24eb
Tehila Naki <72945182+TehilaNaki@users.noreply.github.com>
2022-08-02 22:44:24 +0300

    blimp

--- BL/BLImp.cs
+++ BL/BLImp.cs
@@ -4,11 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
 namespace BL
 {
-    interface IBL
+    class BLImp :IBL
     {
-       // List<ViewFlight> getAllFlights();
     }
 }
