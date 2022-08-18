893c4b8fda1a6fce296542e82ac708e192ffadd6
Tehila Naki <72945182+TehilaNaki@users.noreply.github.com>
2022-08-18 22:53:48 +0300

    use get flight

--- ViewModel/MainWinVM.cs
+++ ViewModel/MainWinVM.cs
@@ -1,13 +1,16 @@
 ﻿using BL;
 using BO;
+using BO.Flights;
 using FlightsMap.ViewModel.Commands;
 using Microsoft.Maps.MapControl.WPF;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Threading;
@@ -21,7 +24,6 @@ namespace FlightsMap.ViewModel
     public MainWinVM()
         {
             ClockSign(1);
-            Push.Add(new Pushpin { Location = new Location(33, 32), ToolTip = "sss" });
             watchCmd = new OpenWatchC();
            
         }
@@ -30,8 +32,8 @@ namespace FlightsMap.ViewModel
         public MainWindow MW { get; set; }
          BLImp bl = new BLImp();
         
-        private List<Pushpin> push=new List<Pushpin>();
-        public List<Pushpin>Push { 
+        private ObservableCollection<Pushpin> push=new ObservableCollection<Pushpin>();
+        public ObservableCollection<Pushpin>Push { 
             get
             {
                 return push;
@@ -45,8 +47,7 @@ namespace FlightsMap.ViewModel
         public string TitleText
         {
             get
-            {
-                
+            {                
                 string holiday = bl.GetNextWeekHolidies().ToUpper();
                 return holiday != "" ? "- Now is a Holiday Week: " + holiday : "";
             }
@@ -60,30 +61,48 @@ namespace FlightsMap.ViewModel
                 PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
             }
         }
-        private void AddPushPine(Flight f)
+        private void AddPushPineIn(FlightInfoPartial f)
         {
             Pushpin p = new Pushpin();
             p.Location = new Location(33, 32);
-            ControlTemplate template = (ControlTemplate)MW.FindResource("pushpin_custom");
+            ControlTemplate template = (ControlTemplate)MW.FindResource("pushpin_customIn");
             Pushpin push = new Pushpin
             {
-                Location = new Location() { Latitude = f.Longtitude, Longitude = f.Latitude },
+                Location = new Location() { Latitude = f.Lat, Longitude = f.Long },
                 Template = template,
-                ToolTip = f.FlightNumber,
+                ToolTip = f.FlightCode,
             };
           //  push.MouseDoubleClick += new MouseButtonEventHandler((sender, e) => Push_MouseEnter(sender, e, f)); ;
             Push.Add(push);
         }
+        //private void AddPushPineOut(FlightInfoPartial f)
+        //{
+        //    Pushpin p = new Pushpin();
+        //    p.Location = new Location(33, 32);
+        //    ControlTemplate template = (ControlTemplate)MW.FindResource("pushpin_customOut");
+        //    Pushpin push = new Pushpin
+        //    {
+        //        Location = new Location() { Latitude = f.Longtitude, Longitude = f.Latitude },
+        //        Template = template,
+        //        ToolTip = f.FlightNumber,
+        //    };
+        //    //  push.MouseDoubleClick += new MouseButtonEventHandler((sender, e) => Push_MouseEnter(sender, e, f)); ;
+        //    Push.Add(push);
+        //}
         private void Refresh()
         {
-           // ControlTemplate template = (ControlTemplate)MW.FindResource("pushpin_custom");
-            Push.Add(new Pushpin { Location = new Location(33, 32), ToolTip = "sss"});
-           // Push.Add(new Pushpin { Location = new Location(34, 32), ToolTip = "sg", Template = template });
-            //List<Flight> l = bl.getAllFlights();
-            //Push.Clear();
-            //foreach (var flight in l)
+            //ControlTemplate template = (ControlTemplate)MW.FindResource("pushpin_custom");            
+            // Push.Add(new Pushpin { Location = new Location(33, 32), ToolTip = "sss" ,Template=template});
+            //Push.Add(new Pushpin { Location = new Location(34, 32), ToolTip = "sg", Template = template });
+            var f = bl.GetCurrentFlights();
+            Push.Clear();
+            foreach (var flight in f["Incoming"])
+            {
+                AddPushPineIn(flight);
+            }
+            //foreach (var flight in f["Outcoming"])
             //{
-            //    AddPushPine(flight);
+            //    AddPushPineOut(flight);
             //}
         }
         private void ClockSign(int seconds)
